c597a19e13f9177ef85d35ddb610ad3852c4e219
R. Brenton Strickler <rbrenton@gmail.com>
2025-04-10 19:31:39 -0400

    Switch to oauth and get everything working again.
    Remove GA

--- cron.php
+++ cron.php
@@ -1,18 +1,23 @@
+#!/usr/bin/php
 <?php
 /**
  * @name UserMap for Google Maps
- * @version 1.0.0 [June 25, 2013]
+ * @version 1.2.0 [April 10, 2025]
  * @author R. Brenton Strickler
- * @fileoverview
- * This script maps users by their home location based on reddit flair.
- */
-
-/**
+ *
+ * @description This script maps users by their home location based on reddit flair.
+ *
+ * @changelog
+ * [June 22, 2020] 1.1.0 Update to use reddit api.
+ * [June 25, 2013] 1.0.0 Initial version. 
+ *
+ * @license Apache 2.0
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,197 +25,304 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 require_once('settings.php');
+
 $conn = @pg_connect(PG_CONNECTION_STRING);
 
 
-function fetchFlairPage() {
-  static $ch = null;
-  static $url = null;
-  static $cookieFile = null;
+function fetchFlairPage($next = null)
+{
+    static $ch = null;
+    static $url = null;
+    static $accessExpiresAt = 0;
+    static $accessToken = null;
+    static $lastAfter = null;
 
-  // Initialize static variables.
-  if($url===null) $url = 'http://www.reddit.com/r/'.REDDIT_SUB.'/about/flair/';
-  if($cookieFile===null) $cookieFile = tempnam('/tmp', 'CURLJAR');
+    // Initialize static variables.
+    if ($url === null) {
+        $url = URL_REDDIT_FLAIR;
+    }
+
+    if ($ch === false || $url === false) {
+        return null;
+    }
+
+    if (0) {
+        $flair = array();
+        $select = pg_query("SELECT name, flair FROM ".PG_TABLE);
+        while ($row = pg_fetch_assoc($select)) {
+            $flair[$row['name']] = $row['flair'];
+        }
+        $ch = false;
+        return $flair;
+    }
+
+    if ($ch === null || $accessExpiresAt <= time() || $accessToken === null) {
+        // Initialize curl
+        if ($ch === null) {
+          $ch = curl_init();
+          curl_setopt($ch, CURLOPT_USERAGENT, CURL_REDDIT_USERAGENT);
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        }
+
+        // Authenticate
+        $fields = array(
+          'grant_type' => 'password',
+          'username' => REDDIT_USERNAME,
+          'password' => REDDIT_PASSWORD
+        );
+        $user = sprintf('%s:%s', REDDIT_API_APP, REDDIT_API_SECRET);
+
+        $_url = URL_REDDIT_ACCESS_TOKEN;
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_URL, $_url);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+        curl_setopt($ch, CURLOPT_USERPWD, $user);//!
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array());
+
+        // Send token request
+        printf("%s:%d fetching url=%s\n", __FUNCTION__, __LINE__, $_url);
+        if (!$ch || !($result = curl_exec($ch))) {
+            $ch = false;
+            return null;
+        }
+
+        printf("%s:%d result=%s\n", __FUNCTION__, __LINE__, $result);
+        $obj = json_decode($result);
+
+        if (!is_object($obj)) {
+          throw new Exception("Failed to parse json object.");
+        }
+
+        if (property_exists($obj, "error")) {
+            printf("%s:%d error=%s error_description=%s\n", __FUNCTION__, __LINE__, $obj->error, property_exists($obj, "error_description") ? $obj->error_description: "");
+            return null;
+        }
+
+        if (!property_exists($obj, "access_token")) {
+            throw new Exception("Failed to find access_token.");
+        }
+
+        $accessToken = $obj->access_token;
+
+        if (!property_exists($obj, "expires_in")) {
+            throw new Exception("Failed to find expires_in.");
+        }
+
+        $accessExpiresAt = time() + floor($obj->expires_in * 0.9);
 
 
-  if($ch===false || $url===false) {
-    return null;
-  }
+        // Reset curl to GET defaults
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+        curl_setopt($ch, CURLOPT_POST, 0);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, null);
+        curl_setopt($ch, CURLOPT_USERPWD, "");
+    }
 
-  if(0) {
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', "Authorization: bearer {$accessToken}"));
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_MAXREDIRS, 3);
+
+    printf("%s:%d fetching url=%s\n", __FUNCTION__, __LINE__, $url);
+    if (!($result = curl_exec($ch))) {
+        $url = false;
+        return null;
+    }
+
+    $obj = json_decode($result);
+
+    printf("%s:%d result=%s\n", __FUNCTION__, __LINE__, $result);
+
+    if (!is_object($obj)) {
+      throw new Exception("Failed to parse json object.");
+    }
+
+    if (property_exists($obj, "error")) {
+        printf("%s:%d error=%s message=%s\n", __FUNCTION__, __LINE__, $obj->error, property_exists($obj, "message") ? $obj->message: "");
+        return null;
+    }
+
+    $after = property_exists($obj, "next") ? $obj->next : null;
+
+    if ($after != null && $lastAfter != $after) {
+      $url = URL_REDDIT_FLAIR."&after={$after}";
+      $lastAfter = $after;
+    } else {
+      $url = false;
+    }
+
     $flair = array();
-    $select = pg_query("SELECT name, flair FROM ".PG_TABLE);
-    while($row = pg_fetch_assoc($select)) {
-      $flair[$row['name']] = $row['flair'];
+
+    foreach ($obj->users as $i => $user) {
+        $flair[$user->user] = $user->flair_text;
     }
-    $ch=false;
+
     return $flair;
-  }
-
-  if($ch===null) {
-    // Initialize curl
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_USERAGENT, 'UserMap 1.0');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_COOKIESESSION, true);
-    curl_setopt($ch, CURLOPT_COOKIEJAR, $cookieFile);
-    curl_setopt($ch, CURLOPT_COOKIEFILE, $cookieFile);
-
-    // Authenticate
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_URL, 'https://ssl.reddit.com/api/login/'.REDDIT_USERNAME);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, array('api_type'=>'json','op'=>'login','passwd'=>REDDIT_PASSWORD,'rem'=>'off','user'=>REDDIT_USERNAME));
-
-    // Send login request
-    if(!$ch || !($result=curl_exec($ch))) {
-      $ch = false;
-      @unlink($cookieFile);
-      return null;
-    }
-
-    // Reset curl to GET defaults
-    curl_setopt($ch, CURLOPT_POST, 0);
-  }
-
-  curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-  curl_setopt($ch, CURLOPT_MAXREDIRS, 3);
-  if(!($result = curl_exec($ch))) {
-    $url = false;
-    @unlink($cookieFile);
-    return null;
-  }
-
-  // Store link to next page
-  $regex = ';<a [^>]*href="([^"]*flair\?after=[^"]+)"[^>]*>next</a>;mi';
-  if(preg_match($regex, $result, $regs)) {
-    $url = $regs[1];
-  } else {
-    $url = false;
-    @unlink($cookieFile);
-  }
-
-  // Parse html from page for user flair.
-  $regex = ';<a [^>]*class="[^"]*author[^"]*flairselectable[^"]*"[^>]*>([^<]+)</a><span class="[^"]*flair[^"]*"[^>]*>([^<]+)</span>;mi';
-  preg_match_all($regex, $result, $regs);
-
-  $flair = array();
-  foreach($regs[1] as $i=>$name) {
-    $flair[$name]=$regs[2][$i];
-  }
-
-  return $flair;
 }
 
-function fetchLatLon($station) {
-  static $ch = null;
-  static $stations = array();
+function rateLimit($key)
+{
+    static $delayUntil = array();
+    $delay = 1.0;
 
-  if($station=='')
-    return null;
+    if (isset($delayUntil[$key])) {
+        $delta = $delayUntil[$key] - microtime(true);
 
-  // First look if the station lat/lon has already been looked up.
-  if(isset($stations[$station]))
+        if ($delta > 0) {
+            printf("%s:%d sleeping=%s ratelimit=%s\n", __FUNCTION__, __LINE__, $delta, $key);
+            usleep($delta);
+        }
+    }
+
+    $delayUntil[$key] = microtime(true) + $delay;
+}
+
+function fetchLatLon($station)
+{
+    static $ch = null;
+    static $stations = array();
+
+    if ($station == '')
+        return null;
+
+    // First look if the station lat/lon has already been looked up.
+    if (isset($stations[$station]))
+        return $stations[$station];
+
+    // Next, see if we already have it in the database.
+    $pgStation = pg_escape_string($station);
+
+    $select = pg_query("SELECT lat,lon FROM ".PG_TABLE." WHERE station='{$pgStation}' AND lat IS NOT NULL AND lon IS NOT NULL LIMIT 1;");
+
+    if ($row = pg_fetch_assoc($select)) {
+        $stations[$station] = array($row['lat'], $row['lon']);
+        printf("%s:%d station=%s, latLon=(%s, %s)\n", __FUNCTION__, __LINE__, $station, $row['lat'], $row['lon']);
+        return $stations[$station];;
+    }
+
+    if ($ch === false) {
+        printf("%s:%d station=%s, latLon=null\n", __FUNCTION__, __LINE__, $station);
+        return null;
+    }
+
+    if ($ch === null) {
+        // Initialize curl
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_USERAGENT, CURL_OTHER_USERAGENT);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        if(!$ch) {
+            $ch = false;
+            printf("%s:%d station=%s, latLon=null\n", __FUNCTION__, __LINE__, $station);
+            return null;
+        }
+    }
+
+    // As a last resort, let's check gcmap.com for the lat/lon.
+    $url = "http://www.gcmap.com/airport/{$station}";
+
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_MAXREDIRS, 3);
+
+    rateLimit("gcmap");
+    if(!($result = curl_exec($ch))) {
+        $url = null;
+        printf("%s:%d station=%s, latLon=null\n", __FUNCTION__, __LINE__, $station);
+        return null;
+    }
+
+    // Parse the html from gcmap.com and send them a thank you card.
+    $regex = ";abbr class=\"latitude\" title=\"([0-9.-]+)\".*?abbr class=\"longitude\" title=\"([0-9.-]+)\";smi";
+    if (preg_match($regex, $result, $regs)) {
+        $lat = $regs[1];
+        $lon = $regs[2];
+
+        printf("%s:%d station=%s, latLon=(%s, %s)\n", __FUNCTION__, __LINE__, $station, $lat, $lon);
+        $stations[$station] = array($lat, $lon);
+    } else {
+        printf("%s:%d station=%s, latLon=null\n", __FUNCTION__, __LINE__, $station);
+        $stations[$station] = null;
+    }
+
+    //todo: send virtual thank you card to gcmap.com
+
     return $stations[$station];
-
-  // Next, see if we already have it in the database.
-  $pgStation = pg_escape_string($station);
-  $select = pg_query("SELECT lat,lon FROM ".PG_TABLE." WHERE station='{$pgStation}' AND lat IS NOT NULL AND lon IS NOT NULL LIMIT 1;");
-  if($row = pg_fetch_assoc($select)) {
-    $stations[$station] = array($row['lat'], $row['lon']);
-    return $stations[$station];
-  }
-
-  if($ch===false) {
-    return null;
-  }
-
-  if($ch===null) {
-    // Initialize curl
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:10.0.2) Gecko/20100101 Firefox/10.0.2');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-    if(!$ch) {
-      $ch = false;
-      return null;
-    }
-  }
-
-  // As a last resort, let's check gcmap.com for the lat/lon.
-  $url = "http://www.gcmap.com/airport/{$station}";
-
-  curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-  curl_setopt($ch, CURLOPT_MAXREDIRS, 3);
-
-  if(!($result = curl_exec($ch))) {
-    $url = null;
-    return null;
-  }
-
-  // Parse the html from gcmap.com and send them a thank you card.
-  $regex = ";abbr class=\"latitude\" title=\"([0-9.-]+)\".*?abbr class=\"longitude\" title=\"([0-9.-]+)\";smi";
-  if(preg_match($regex, $result, $regs)) {
-    $lat = $regs[1];
-    $lon = $regs[2];
-
-    $stations[$station] = array($lat, $lon);
-  } else {
-    $stations[$station] = null;
-  }
-
-  //todo: send virtual thank you card to gcmap.com
-
-  return $stations[$station];
 }
 
+function updatePilot($name, $flair)
+{
+    $forceLatLonUpdate = true;
 
-$pilots = array();
-while($flairs = fetchFlairPage()) {
-  foreach($flairs as $user=>$flair) {
-    $base = '';
-    if(preg_match(';[^A-Z0-9]([A-Z0-9]{3,4})[)]?$;', trim($flair), $regs)) {
-      $base = $regs[1];
-      if(preg_match(';^(SIM|ST|SPT|RPL|PPL|CPL|ATP|MIL|ATC|CFI|MEI|ABI|AB|CMP|HP|IR|TW|GLI|MEL|MES|ROT|SEL|SES|ASEL|ASES|SELS|INST|CFII)$;', $base))
-        $base = '';
-    } else if(preg_match(';[(]([0-9.-]+),([0-9.-]+)[)];', trim($flair), $regs)) {
-      $base = '';//$base = 'n/a';
-      $lat = (double) $regs[1];
-      $lon = (double) $regs[2];
-      if($lat==0 || $lon==0) $lat = $lon = 0;
+    $station = getStation($flair);
+
+    $pgName = pg_escape_string($name);
+    $pgStation = pg_escape_string($station);
+    $pgFlair = pg_escape_string($flair);
+
+    $sql = sprintf("SELECT name, station, lat, lon, locked FROM %s WHERE name = '%s';", PG_TABLE, $pgName);
+    $select = pg_query($sql);
+
+    $latLon = ($forceLatLonUpdate || pg_num_rows($select) == 0) ? fetchLatLon($station) : null;
+
+    if (is_array($latLon)) {
+        $lat = (double) $latLon[0];
+        $lon = (double) $latLon[1];
+    } else {
+        $lat = 'null';
+        $lon = 'null';
     }
-    if(!isset($pilots[$user])) {
-      $pilots[$user] = array($flair,$base);
+
+    $count = pg_num_rows($select);
+    printf("%s:%d station=%s, name=%s, lat=%s, lon=%s, flair=%s, rows=%s\n", __FUNCTION__, __LINE__, $station, $name, $lat, $lon, $flair, $count);
+    if ($count == 0) {
+        $sql = sprintf("INSERT INTO %s (name, station, lat, lon, flair, time_updated) VALUES ('%s', '%s', %s, %s, '%s', NOW());", PG_TABLE, $pgName, $pgStation, $lat, $lon, $pgFlair);
+        printf("%s:%d sql=%s\n", __FUNCTION__, __LINE__, $sql);
+        //$insert = pg_query($sql);
+    } else {
+        $row = pg_fetch_assoc($select);
+        if ($forceLatLonUpdate || $row['station'] != $station) {
+            if ($row['locked'] == 'f' && ($row['station'] != 'n/a' || $station != '')) {
+                $sql = sprintf("UPDATE %s SET time_updated = NOW(), station = '%s', flair = '%s', lat = %s, lon = %s WHERE name = '%s';", PG_TABLE, $pgStation, $pgFlair, $lat, $lon, $pgName);
+                printf("%s:%d sql=%s\n", __FUNCTION__, __LINE__, $sql);
+                //$update = pg_query($sql);
+            }
+        }
     }
-  }
 }
 
-$forceLatLonUpdate = true;
-foreach($pilots as $user=>$data) {
-  list($flair,$base) = $data;
-  $pgName = pg_escape_string($user);
-  $pgStation = pg_escape_string(strtoupper($base));
-  $pgFlair = pg_escape_string($flair);
-  $select = pg_query("SELECT name,station,lat,lon,locked FROM ".PG_TABLE." WHERE name='{$pgName}';");
+function getStation($flair)
+{
+    $station = '';
 
-  $latLon = ($forceLatLonUpdate||pg_num_rows($select)==0) ? fetchLatLon($pgStation) : null;
-  if(is_array($latLon)) {
-    $lat = (double) $latLon[0];
-    $lon = (double) $latLon[1];
-  } else {
-    $lat = 'null';
-    $lon = 'null';
-  }
+    if (preg_match(';[^A-Z0-9]([A-Z0-9]{3,4})[)]?$;', trim($flair), $regs)) {
+        $station = $regs[1];
+        if (preg_match(';^(SIM|ST|SPT|RPL|PPL|CPL|ATP|MIL|ATC|CFI|MEI|ABI|AB|CMP|HP|IR|TW|GLI|MEL|MES|ROT|SEL|SES|ASEL|ASES|SELS|INST|CFII)$;', $station))
+            $station = '';
+    } else if (preg_match(';[(]([0-9.-]+),([0-9.-]+)[)];', trim($flair), $regs)) {
+        $station = ''; //$station = 'n/a';
+        $lat = (double) $regs[1];
+        $lon = (double) $regs[2];
 
-  if(pg_num_rows($select)==0) {
-    pg_query("INSERT INTO ".PG_TABLE." (name, station, lat, lon, flair) VALUES ('{$pgName}', '{$pgStation}', {$lat}, {$lon}, '{$pgFlair}');");
-  } else {
-    $row = pg_fetch_assoc($select);
-    if($forceLatLonUpdate||$row['station']!=$base) {
-      if(!$row['locked'] && ($row['station']!='n/a' || $base!=''))
-        pg_query("UPDATE ".PG_TABLE." SET station='{$pgStation}',flair='{$pgFlair}',lat={$lat},lon={$lon} WHERE name='{$pgName}';");
+        if ($lat == 0 || $lon == 0) {
+            $lat = $lon = 0;
+        }
     }
-  }
+
+    return strtoupper($station);
+}
+
+$dupes = array();
+
+while ($flairs = fetchFlairPage()) {
+    foreach ($flairs as $name => $flair) {
+        if (isset($dupes[$name])) continue;
+
+        updatePilot($name, $flair);
+
+        $dupes[$name] = true;
+    }
 }
--- index.php
+++ index.php
@@ -27,14 +27,14 @@ function echoStationJSON($station, $pilots) {
   $lat = (double) $pilots[0]['lat'];
   $lon = (double) $pilots[0]['lon'];
 
-  if(preg_match(';^[A-Z0-9]{3,4}$;', $station)) {
+  if (preg_match(';^[A-Z0-9]{3,4}$;', $station)) {
     $html = "<a href=\"http://www.gcmap.com/airport/{$station}\">{$station}</a><br/><br/>";
   } else {
     $html = "{$station}<br/><br/>";
   }
 
   $count = 0;
-  foreach($pilots as $row) {
+  foreach ($pilots as $row) {
     $count++;
     $name = htmlspecialchars($row['name']);
     $flair = htmlspecialchars($row['flair']);
@@ -44,7 +44,7 @@ function echoStationJSON($station, $pilots) {
   $stationVar = $tmp = preg_replace(';[^A-z0-9];', '', $station);
   static $stationVars = array();
   $i=0;
-  while(isset($stationVars[$tmp])) {
+  while (isset($stationVars[$tmp])) {
     $tmp = $stationVar.'_'.($i++);
   }
   $stationVar = $tmp;
@@ -53,7 +53,7 @@ function echoStationJSON($station, $pilots) {
   echo json_encode(array('title'=>"{$stationVar} - {$count}",'count'=>$count,'lon'=>$lon,'lat'=>$lat, 'html'=>$html)).',';
 }
 
-switch($_GET['a']) {
+switch ($_GET['a']) {
 case 'data.json':
   $conn = @pg_connect(PG_CONNECTION_STRING);
   $stations = array();
@@ -62,25 +62,38 @@ case 'data.json':
 var data = [
 JAVASCRIPT;
 
-  $select = pg_query("SELECT name,station,lat,lon,flair FROM ".PG_TABLE." WHERE station!='' AND lat IS NOT NULL ORDER BY station='n/a',station,lat,lon;");
+  $select = pg_query("SELECT name,station,lat,lon,flair FROM ".PG_TABLE." WHERE station!='' AND lat IS NOT NULL ORDER BY lat,lon,station='n/a',length(station) desc,station;");
   $count = pg_num_rows($select);
   $lastStation = null;
+  $lastLatLon = null;
   $pilots = array();
-  for($i=0; $i<$count; $i++) {
+  for ($i = 0; $i < $count; $i++) {
     $row = pg_fetch_assoc($select);
-    if($lastStation==null) {
+    $station = $row['station'];
+    $latLon = "{$row['lat']}:{$row['lon']}";
+
+    if ($lastStation === null && $lastLatLon === null) {
+      // New group.
       $pilots = array();
-      $lastStation = $row['station'];
+      $lastStation = $station;
+      $lastLatLon = $latLon;
     }
-    if($lastStation!=$row['station']) {
+    // Output last group.
+    if ($lastLatLon != $latLon) {
       echoStationJSON($lastStation, $pilots);
+
+      // New group.
       $pilots = array();
+      $lastStation = $station;
+      $lastLatLon = $latLon;
     }
+
+    // Add to current group.
     $pilots[] = $row;
-    if($i+1>=$count) {
-      echoStationJSON($row['station'], $pilots);
+    if ($i + 1 >= $count) {
+      // Output very last group.
+      echoStationJSON($station, $pilots);
     }
-    $lastStation=$row['station'];
   }
   echo <<<JAVASCRIPT
 ];
@@ -97,11 +110,11 @@ $defaultLat = 37.0625;
 $defaultLon = -95.677068;
 $defaultZoom = 2;
 
-if($_GET['name']!='') {
+if ($_GET['name']!='') {
   $conn = @pg_connect(PG_CONNECTION_STRING);
   $nameSQL = pg_escape_string($_GET['name']);
   $select = pg_query("SELECT name,station,lat,lon,flair FROM ".PG_TABLE." WHERE name='{$nameSQL}' AND lat IS NOT NULL LIMIT 1;");
-  if(pg_num_rows($select)>0) {
+  if (pg_num_rows($select) > 0) {
     $row = pg_fetch_assoc($select);
     $defaultLat = $row['lat'];
     $defaultLon = $row['lon'];
@@ -114,71 +127,89 @@ $header = HTML_HEADER;
 ?>
 <html>
 <head>
-<meta name="viewport" content="initial-scale=1.0, user-scalable=no"/>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
-<title><?php echo $title; ?></title>
-<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
-<script type="text/javascript" src="?a=data.json"></script>
-<script type="text/javascript" src="js/markerclusterer.js"></script>
-<link rel="stylesheet" href="css/default.css" type="text/css"/>
+  <meta name="viewport" content="initial-scale=1.0, user-scalable=no"/>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+  <title><?php echo $title; ?></title>
+  <link rel="stylesheet" href="css/default.css" type="text/css"/>
 </head>
 <body>
-<div id="header"><?php echo $header; ?></div>
-<div id="map_canvas" style="width:100%; height:100%"></div>
+  <div id="header"><?php echo $header; ?></div>
+  <div id="map_canvas" style="width:100%; height:100%"></div>
+  <script type="text/javascript" src="//code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8=" crossorigin="anonymous"></script>
+  <script type="text/javascript" src="//maps.google.com/maps/api/js?key=AIzaSyBnc3A-F4S2J9l2A_1bez6q8fsbg97ZBDI&sensor=false"></script>
+  <script type="text/javascript" src="?a=data.json"></script>
+  <script type="text/javascript" src="js/markerclusterer.js"></script>
+  <script type="text/javascript">
+  function initialize() {
+    function bindInfoWindow(marker, map, infoWindow, html) {
+        google.maps.event.addListener(marker, 'click', function() {
+          infoWindow.setContent(html);
+          infoWindow.open(map, marker);
+      });
+    }
+    function fnPilotCount(markers, numStyles) {
+      var index = 0;
+      var count = 0;
 
-<script type="text/javascript">
-function initialize() {
-  function bindInfoWindow(marker, map, infoWindow, html) {      
-      google.maps.event.addListener(marker, 'click', function() {
-        infoWindow.setContent(html);
-        infoWindow.open(map, marker);
+      for (var i=0; i<markers.length; i++) {
+        count = count + markers[i].pilots;
+      }
+      count = count.toString();
+
+      var dv = count;
+      while (dv !== 0) {
+        dv = parseInt(dv / 10, 10);
+        index++;
+      }
+
+      index = Math.min(index, numStyles);
+      return {
+        text: count,
+        index: index
+      };
+    };
+
+
+    var latlng = new google.maps.LatLng(<?php echo $defaultLat;?>, <?php echo $defaultLon;?>);
+
+
+    var mapOptions = {
+      zoom: <?php echo $defaultZoom;?>,
+      center: latlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP //'http://tile.openstreetmap.org/{z}/{x}/{y}.png'//google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+
+    var infowindow = new google.maps.InfoWindow( { backgroundColor: '#333'} );
+    var markers = [];
+    var clusterStyle = [
+      { url: 'images/m1.png', height: 52, width: 53 },
+      { url: 'images/m2.png', height: 55, width: 56 },
+      { url: 'images/m3.png', height: 65, width: 66 },
+      { url: 'images/m4.png', height: 77, width: 78 },
+      { url: 'images/m5.png', height: 89, width: 90 },
+    ];
+    for (var i = 0; i < data.length; i++) {
+      var markerLatLng = new google.maps.LatLng(data[i].lat, data[i].lon);
+      var marker = new google.maps.Marker({position: markerLatLng, title: data[i].title});
+      marker.pilots = data[i].count;
+      markers.push(marker);
+      bindInfoWindow(marker, map, infowindow, data[i].html);
+    }
+    var markerCluster = new MarkerClusterer(map, markers, {styles: clusterStyle, gridSize: 50, maxZoom: 10, calculator: fnPilotCount});
+
+    var e = [];
+    function s2k(s) { return s.replace('!3m9', '').replace('!2m3!1e2!6m1!3e5!3m14', '').replace(/!12m4!1e26!2m2!1sstyles!2z[^!]+/, '').replace(/&key=.*/, ''); }
+    function b2s(b) { var k = s2k(b); return (typeof e[k] != 'undefined') ? e[k] : b.replace('!2m3!1e2!6m1!3e5!3m14', '!3m9'); }
+    $('#map_canvas').on('DOMSubtreeModified', 'div[tabindex=0]', function() {
+      var el = $(this);
+      el.find('img[src*=3m9]').each(function() { var s = $(this).attr('src'); e[s2k(s)] = s; });
+      el.find('img[src*=2m3]').each(function() { var b = $(this).attr('src'); $(this).attr('src', b2s(b)); });
     });
   }
-  function fnPilotCount(markers, numStyles) {
-    var index = 0;
-    var count = 0;
 
-    for(var i=0; i<markers.length; i++) {
-      count = count + markers[i].pilots;
-    }
-    count = count.toString();
-
-    var dv = count;
-    while (dv !== 0) {
-      dv = parseInt(dv / 10, 10);
-      index++;
-    }
-
-    index = Math.min(index, numStyles);
-    return {
-      text: count,
-      index: index
-    };
-  };
-
-
-  var latlng = new google.maps.LatLng(<?php echo $defaultLat;?>, <?php echo $defaultLon;?>);
-
-
-  var map = new google.maps.Map(document.getElementById('map_canvas'), {
-    zoom: <?php echo $defaultZoom;?>,
-    center: latlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  });
-
-  var infowindow = new google.maps.InfoWindow( { backgroundColor: '#333'} );
-  var markers = [];
-  for (var i = 0; i < data.length; i++) {
-    var markerLatLng = new google.maps.LatLng(data[i].lat, data[i].lon);
-    var marker = new google.maps.Marker({position: markerLatLng, title: data[i].title});
-    marker.pilots = data[i].count;
-    markers.push(marker);
-    bindInfoWindow(marker, map, infowindow, data[i].html);
-  }
-  var markerCluster = new MarkerClusterer(map, markers, {gridSize: 50, maxZoom: 10, calculator: fnPilotCount});
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
-</script>
+  google.maps.event.addDomListener(window, 'load', initialize);
+  </script>
 </body>
 </html>
